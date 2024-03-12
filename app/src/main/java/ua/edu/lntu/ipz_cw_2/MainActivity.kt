eec43d90ab826c5b511a37e9e52a4894a6a48987
eff1c <illia.martyniuk@busmarket.ua>
2024-03-12 14:24:41 +0200

    Add sing in preview

--- app/src/main/java/ua/edu/lntu/ipz_cw_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_2/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ipz_cw_2.ui.theme.IPZ_CW_2_Martyniuk_IlliaTheme
@@ -29,4 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SignIn(modifier: Modifier = Modifier) {}
+fun SignIn(modifier: Modifier = Modifier) {
+    Column {
+        TextField(value = "", onValueChange = {}, placeholder = { Text("Введіть email") })
+        TextField(value = "", onValueChange = {}, placeholder = { Text("Введіть пароль") })
+        Button(onClick = {}) { Text(text = "Ввійти") }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignInPreview() {
+    IPZ_CW_2_Martyniuk_IlliaTheme {
+        SignIn()
+    }
+}
